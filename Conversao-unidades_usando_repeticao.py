ee08cf302bc2386faabffbc37162c88874e3615e
unknown <ian.pinho.tsis@gmail.com>
2023-04-20 16:09:52 -0300

    adicionei os calculos das unidades byte até Pentabytes, tanto multiplicação tanto quanto divisão usando range pra multiplicar de acordo com a distância das unidades na lista.

--- Conversao-unidades_usando_repeticao.py
+++ Conversao-unidades_usando_repeticao.py
@@ -17,9 +17,12 @@ def Conversor (valorAConverter,unidadeConvertida,unidadeDesejada,):
     if unidades.index(unidadeConvertida)<unidades.index(unidadeDesejada):
         for distancia in range(unidades.index(unidadeConvertida),unidades.index(unidadeDesejada)):
             valorFinal=valorFinal/1024
+    else:
+        for distancia in range(unidades.index(unidadeConvertida),unidades.index(unidadeDesejada),-1):
+            valorFinal=valorFinal*1024
+        
     print(valorFinal)
 Conversor(valorAConverter,unidadeConvertida,unidadeDesejada)
-    
 
 
 
@@ -31,3 +34,32 @@ Conversor(valorAConverter,unidadeConvertida,unidadeDesejada)
 
 
 
+
+
+
+# def converter(quantidade, unidade_origem, unidade_destino):
+#     indice_origem = unidades.index(unidade_origem)
+#     indice_destino = unidades.index(unidade_destino)
+
+#     if indice_origem > indice_destino:
+#         quantidade = converter(quantidade, unidade_origem, unidades[indice_destino + 1])
+#         indice_origem = indice_destino + 1
+
+#     fator = 1024 ** (indice_destino - indice_origem)
+
+#     return quantidade * fator
+
+# def conversao_multipla(quantidade, unidade_origem, unidade_destino):
+#     if unidades.index(unidade_destino) < unidades.index(unidade_origem):
+#         unidade_origem, unidade_destino = unidade_destino, unidade_origem
+#     resultado = quantidade
+#     while unidade_origem != unidade_destino:
+#         proxima_unidade = unidades[unidades.index(unidade_origem) + 1]
+#         resultado = converter(resultado, unidade_origem, proxima_unidade)
+#         unidade_origem = proxima_unidade
+#     return resultado
+# print("Insira o valor a ser convertido, em seguida a unidade desejada e por ultimo a unidade a ser convertida:")
+
+# result=conversao_multipla(int(input()), input(), input())
+
+# print(result)
